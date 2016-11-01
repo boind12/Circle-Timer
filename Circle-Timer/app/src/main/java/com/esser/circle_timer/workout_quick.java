f54589c03450c251e8e7997ab85e2c684f2873e6
boind12 <boind12@gmail.com>
2016-11-01 13:15:25 +0100

    Get Data from MainAcitivity

--- Circle-Timer/app/src/main/java/com/esser/circle_timer/workout_quick.java
+++ Circle-Timer/app/src/main/java/com/esser/circle_timer/workout_quick.java
@@ -1,7 +1,9 @@
 package com.esser.circle_timer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class workout_quick extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class workout_quick extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout_quick);
+
+        //Get Data from MainActivity
+        int rounds=1, workinterval=1, pauseinterval=1;
+        Intent main_Intent = new Intent();
+        Bundle main_Bundle = main_Intent.getExtras();
+        rounds= main_Bundle.getInt("QUICK_ROUNDS");
+        workinterval=main_Bundle.getInt("QUICK_WORKINTERVAL");
+        pauseinterval=main_Bundle.getInt("QUICK_PAUSEINTERVAL");
     }
 }
