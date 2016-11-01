ea2a5651d9196c8c0520862e5123708f232d2984
boind12 <boind12@gmail.com>
2016-11-01 12:27:35 +0100

    Added new layout file
    added new layout file the correct-way and applied old layout

---
+++ Circle-Timer/app/src/main/java/com/esser/circle_timer/workout_quick.java
@@ -0,0 +1,13 @@
+package com.esser.circle_timer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class workout_quick extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workout_quick);
+    }
+}
