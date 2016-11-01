87f0d3c427b9c54cb2ba56de7c538436fd07073d
boind12 <boind12@gmail.com>
2016-11-01 13:36:32 +0100

    Added Inititial Countdown

--- Circle-Timer/app/src/main/java/com/esser/circle_timer/workout_quick.java
+++ Circle-Timer/app/src/main/java/com/esser/circle_timer/workout_quick.java
@@ -1,16 +1,27 @@
 package com.esser.circle_timer;
 
 import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class workout_quick extends AppCompatActivity {
+
+    //Declare Variables
+    private CountDownTimer Timer;
+    private TextView tV_CountDown;
+    private TextView tV_Message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout_quick);
+
+        //Apply Variables
+        tV_CountDown=(TextView)findViewById(R.id.tV_CountDown);
 
         //Get Data from MainActivity
         int rounds=1, workinterval=1, pauseinterval=1;
@@ -19,5 +30,25 @@ public class workout_quick extends AppCompatActivity {
         rounds= main_Bundle.getInt("QUICK_ROUNDS");
         workinterval=main_Bundle.getInt("QUICK_WORKINTERVAL");
         pauseinterval=main_Bundle.getInt("QUICK_PAUSEINTERVAL");
+
+        //Start Timer
+        Timer = new CountDownTimer(5000,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                tV_CountDown.setText("" + millisUntilFinished / 1000);
+            }
+
+            @Override
+            public void onFinish() {
+                working_out();
+            }
+        }.start();
+    }
+
+    public void working_out()
+    {
+        //Apply Variables
+        tV_Message=(TextView)findViewById(R.id.tV_Rounds);
+        tV_Message.setText(getResources().getString(R.string.tV_Message_Go));
     }
 }
